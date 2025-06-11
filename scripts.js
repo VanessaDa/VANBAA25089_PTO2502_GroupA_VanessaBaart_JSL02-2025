07c05b14a6a005b7fbc995869472e99992c550b9
Vanessa Gwama <vanessagwama@MacBookAir.home>
2025-06-11 09:57:25 +0200

    Add task details input modal

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Prompt user for full task details and validate status input
+  function getTaskDetails(taskNumber) {
+    const taskTitle = prompt(`Enter the title for Task ${taskNumber}:`);
+    const taskDescription = prompt(`Enter the description for Task ${taskNumber}:`);
+
+  }
+})
